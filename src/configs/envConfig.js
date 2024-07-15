32802fa0dc999d63e2e217a3f2552b6ff57fcd97
GodoyMS <peruvalmunidial@gmail.com>
2024-07-15 08:23:27 -0500

    added new backend ulr

--- src/configs/envConfig.js
+++ src/configs/envConfig.js
@@ -1,2 +1,1 @@
-export const BACKEND="https://squid-app-vvma9.ondigitalocean.app/api";//  http://localhost:1337/api https://api.dev.pupicat.com/api  
-export const IMAGEBASEPATH="https://api.pupicat.com";//  http://localhost:1337 https://api.dev.pupicat.com 
+export const BACKEND="https://mc-trabajadores-server-foewg.ondigitalocean.app/api";//  http://localhost:1337/api https://api.dev.pupicat.com/api  
