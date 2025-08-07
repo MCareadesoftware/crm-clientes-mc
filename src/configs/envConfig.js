30b72fc5942c9a4d520e07ec08d70b67e67dd4ab
mcprogrmador2 <monstruocreativo.programador2@gmail.com>
2025-08-07 09:23:23 -0500

    Environment variables

--- src/configs/envConfig.js
+++ src/configs/envConfig.js
@@ -1,1 +1,1 @@
-export const BACKEND="https://mc-trabajadores-server-foewg.ondigitalocean.app/api";//  http://localhost:1337/api https://api.dev.pupicat.com/api  
+export const BACKEND = process.env.BACKEND;
